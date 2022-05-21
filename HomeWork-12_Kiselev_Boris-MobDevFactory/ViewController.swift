6fbe4d5087564a9314bdaab2dd48fd9ae7475d5e
BorisKiselev <hugh.dancy@icloud.com>
2022-05-21 18:14:27 +0700

    Initial Commit

---
+++ HomeWork-12_Kiselev_Boris-MobDevFactory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-12_Kiselev_Boris-MobDevFactory
+//
+//  Created by Борис Киселев on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
